d0e6f5593d76cb30adc7dd022c11d0f1b9e60654
Daniel <daniel.herrera2292@gmail.com>
2023-01-26 14:59:00 -0400

    Register new vm success

--- utils.py
+++ utils.py
@@ -1,1 +1,60 @@
 import paramiko
+import os 
+from dotenv import load_dotenv
+import mysql.connector
+from pathlib import Path
+
+load_dotenv()
+
+def connectDb():
+  try:
+    mydb = mysql.connector.connect(
+      host = os.getenv('dbhost'),
+      user = os.getenv('userdb'),
+      password ='',
+      database = os.getenv('dbname'),
+      auth_plugin = "mysql_native_password"
+    )
+    return mydb
+  except OSError:
+    return OSError
+
+
+def searchs_vm():
+  transport = paramiko.Transport((os.getenv('host'),int(os.getenv('port'))))
+  transport.connect(username=os.getenv('usernameh'), password=os.getenv('password'))
+  sftp = paramiko.SFTPClient.from_transport(transport)
+  vmlist = sftp.listdir('/var/spool/asterisk/voicemail/default/7599/INBOX')
+  sftp.close()
+  transport.close()
+  return vmlist
+
+
+
+def is_audio(msj):
+  return msj if msj.endswith('.wav') else False
+
+def not_in_the_list(x,list):
+  return x not in list
+
+def check_database(vmlist):
+  mydb = connectDb()
+  myCursor = mydb.cursor()
+  myCursor.execute("SELECT filename from voice_mail ;")
+  vmsaved = [ x[0] for x in myCursor.fetchall()]
+  vmnews = list(filter(lambda x: not_in_the_list(x,vmsaved), vmlist))
+  for newvm in vmnews:
+    querySQL = "INSERT INTO voice_mail (filename, dateregister) VALUES (%s, current_date());"
+    myCursor.execute(querySQL, (newvm,))
+    mydb.commit()
+  return len(vmnews)
+    
+     
+
+def check_vm():
+  vmlist = searchs_vm()
+  vmlist = list(filter(is_audio,vmlist))
+  return check_database(vmlist)
+
+print(check_vm())
+
